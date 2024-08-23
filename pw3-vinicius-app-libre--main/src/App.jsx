fec66e82416bba8fb7750258f997cb96f6fa55f5
Vinicius Patriota <vinicius.patriota3101@gmail.com>
2024-08-22 22:28:33 -0300

    Inclusão do sistema de NavBar

---
+++ pw3-vinicius-app-libre--main/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+// import CardBooks from './components/CardBooks'
+import NavBar from './components/layout/NavBar'
+import capaLivro from './assets/cavernas_aco.jpg'
+ 
+function App() {
+ 
+ 
+  return (
+    // <div className='nsei' >
+
+    //   <div>
+    //    <CardBooks
+    //   titulo ='Cavernas de Aço'
+    //   autor= 'Isaac Asimov'
+    //   imagem={capaLivro}
+    //   />
+    //   </div>
+    //   <div>
+    //   </div>
+    // </div>
+    <NavBar></NavBar>
+  
+  )
+}
+ 
+export default App
